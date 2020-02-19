56717aac431f8fbfc0db5b97409776b9c0078b1a
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-02-19 14:47:24 -0800

    set columns and rows for refuge output

--- src/safeSpaceInfo.js
+++ src/safeSpaceInfo.js
@@ -1,12 +1,12 @@
 export class SafeSpaceInfo {
     constructor(safeSpace) {
         this.safeSpace = safeSpace
-        this.name = "NAME: " + safeSpace.name;
-        this.address = "ADDRESS: " + safeSpace.street + safeSpace.city + safeSpace.state;
-        this.directions = "DIRECTIONS: " + safeSpace.directions;
-        this.distance = "DISTANCE FROM YOUR LOCATION: " + (safeSpace.distance).toFixed(2) + "MILES";
+        this.name = '<strong>' + "NAME: " + '</strong>' + safeSpace.name;
+        this.address = '<strong>' + "ADDRESS: " + '</strong>' + safeSpace.street + safeSpace.city + safeSpace.state;
+        this.directions = '<strong>' + "DIRECTIONS: " + '</strong>' + safeSpace.directions;
+        this.distance = '<strong>' + "DISTANCE FROM YOUR LOCATION: " + '</strong>' + (safeSpace.distance).toFixed(2) + "MILES";
         this.latitude = safeSpace.latitude;
         this.longitude = safeSpace.longitude; 
-        this.accessible = "ADA ACCESSIBLE? " + safeSpace.accessible;
+        this.accessible = '<strong>' + "ADA ACCESSIBLE? " + '</strong>' + safeSpace.accessible;
     }
   }
